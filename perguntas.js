45f441d537f8cc812feda14c7872aaa5eb6f5bd7
larymaris <161326532+larymaris@users.noreply.github.com>
2024-10-11 08:25:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quimica organica',
+    'O que sao hidrocarboneto?',
+    'sao compostos organicos formados apenas por apenas por atomos de carbono e hidrogenio'
+)
+
+criaCartao(
+    'quimica organica',
+    'O que e uma reaçao de esterificaçao?',
+    'E reaçao entre um acido carboxilico e um alcool, formando um ester e agua'
+)
+
+criaCartao(
+    'Eletroquimica',
+    'O que e potencial de reuçao?',
+    'E a tendencia de uma especie quimica ganhar eletrons, medida em volts.'
+)
+
+criaCartao(
+    'Eletroquimica',
+    'O que e eletrolito?',
+    'E uma substancia que quando dissolvida em agua, dissocia-se em ions, permitindo a conduçao de corrente eletrica.'
+) 
